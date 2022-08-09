756bf3f0646372aa1859f0f221bb119dfd6caeca
Robert Manteghi <unruffled.nightingale@gmail.com>
2022-08-09 18:11:33 +0100

    fix_for_poetry_version (#15)

--- fastapi_template/__init__.py
+++ fastapi_template/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.0"
+from pathlib import Path
+
+import toml
+
+
+def get_version() -> str:
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    version: str = pyproject["tool"]["poetry"]["version"]
+    return version
+
+
+__version__ = get_version()
